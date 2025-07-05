4c174a3bf0b79ba67d56059658618783ad75f815
Md. Sazzad Mia <121119379+MohammadSazzad@users.noreply.github.com>
2025-07-05 23:15:20 +0600

    Missing file is added

---
+++ content/mailPredictionWebApp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  5 18:31:08 2025
+
+@author: sazzad
+"""
+
+import numpy as np
+import pickle
+import os
+import streamlit as st
+
+model_path = os.path.expanduser('~/Documents/ML_Project/trained_mail_model.sav')
+
+with open(model_path, 'rb') as f:
+        artifacts = pickle.load(f)
+        model = artifacts['model']
+        vectorizer = artifacts['vectorizer']
+        
+def mail_prediction(input_mail):
+    input_features = vectorizer.transform([input_mail] if isinstance(input_mail, str) else input_mail)
+    prediction = model.predict(input_features)
+
+    if (prediction[0] == 0):
+      return 'Spam'
+    else:
+      return 'Ham'
+      
+def main():
+    st.title("Email prediction Web App")
+    input_mail = st.text_input("Insert the mail")
+    
+    result = '';
+    
+    if st.button("Email test result"):
+        result = mail_prediction(input_mail)
+        
+    st.success(result)
+    
+    
+if __name__ == '__main__' :
+    main()
